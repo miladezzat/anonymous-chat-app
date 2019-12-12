c5d2f63a53ebcad8f6379301412737baa097c2f1
Milad E. Fahmy <milad.ez.fahmy@gmail.com>
2019-12-12 21:18:22 +0200

    App

--- app.js
+++ app.js
@@ -18,27 +18,27 @@ app.get('/', function(req, res) {
 app.use(express.static(__dirname + '/public'));
 
 
-users = [];
-io.on('connection', function(socket) {
-    socket.on('setUsername', function(data) {
-        if (users.indexOf(data) > -1) {
-            socket.emit('userExists', data + ' username is taken! Try some other username.');
-        } else {
-            users.push(data);
-            socket.emit('userSet', { username: data });
-        }
-        socket.on('disconnect', function() {
-            let newUsers = users.filter(el => {
-                return el != data
-            });
-            users = newUsers;
-        });
-    });
+// users = [];
+// io.on('connection', function(socket) {
+//     socket.on('setUsername', function(data) {
+//         if (users.indexOf(data) > -1) {
+//             socket.emit('userExists', data + ' username is taken! Try some other username.');
+//         } else {
+//             users.push(data);
+//             socket.emit('userSet', { username: data });
+//         }
+//         socket.on('disconnect', function() {
+//             let newUsers = users.filter(el => {
+//                 return el != data
+//             });
+//             users = newUsers;
+//         });
+//     });
 
-    socket.on('msg', function(data) {
-        io.sockets.emit('newmsg', data);
-    })
-});
+//     socket.on('msg', function(data) {
+//         io.sockets.emit('newmsg', data);
+//     })
+// });
 
 http.listen(PORT, function() {
     console.log('listening on localhost:3000');
