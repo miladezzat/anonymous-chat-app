369f6644e3d3c863cab7d688d7cbea23f31862e2
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-07-04 18:29:29 +0200

    updated

--- public/chat.js
+++ public/chat.js
@@ -1,36 +1,46 @@
-var socket = io();
+const socket = io();
 
 function setUsername() {
     let userName = document.getElementById('name').value;
     socket.emit('setUsername', userName);
     document.getElementById('userTitle').textContent = userName;
 };
-var user;
-socket.on('userExists', function(data) {
+
+let user;
+socket.on('userExists', function (data) {
     document.getElementById('error-container').innerHTML = data;
 });
-socket.on('userSet', function(data) {
+
+socket.on('userSet', function (data) {
     document.getElementById('login').remove();
     let messageContainer = document.getElementById('messageContainer');
     user = data.username;
     messageContainer.innerHTML = `
-    <div class="form-group">
+        <div id = "message-container"></div>
+        <div class="fixed-bottom  w-25 mx-auto mb-5">
+        
+        <div class="form-group">
         <input class="form-control" type = "text" id = "message" placeholder="Enter your message">
-    </div>
-    <button class="btn btn-success btn-block" type = "button" name = "button" onclick = "sendMessage()">Send</button>
-     <div id = "message-container"></div>`;
+        </div>
+        <button class="btn btn-success btn-block" type = "button" name = "button" onclick = "sendMessage()">Send</button>
+        </div>
+    `;
 });
 
 function sendMessage() {
-    var msg = document.getElementById('message').value;
+    const msg = document.getElementById('message').value;
     if (msg) {
         socket.emit('msg', { message: msg, user: user });
     }
     document.getElementById("message").value = "";
 }
-socket.on('newmsg', function(data) {
+
+socket.on('newmsg', function (data) {
     if (user) {
         document.getElementById('message-container').innerHTML += '<div><b>' +
-            data.user + '</b>: ' + data.message + '</div>'
+            data.user + '</b>: ' + data.message + '</div>';
+
+        const myDiv = document.getElementById("message-container");
+        myDiv.scrollTop = myDiv.scrollHeight;
     }
-})
+});
